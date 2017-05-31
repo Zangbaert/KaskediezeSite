495561bbc8c27d9e7e9c8c7fdc66d409a1753b1d
Timo <tkrijnen@xs4all.nl>
2017-05-31 19:28:02 +0200

    Ook vergeten  hoofdletter

--- kasketest/photoswipe/filmopname.js
+++ kasketest/photoswipe/filmopname.js
@@ -6,47 +6,47 @@ var openPhotoSwipe = function() {
     // build items array
     var items = [
         {
-            src: '../Fotos/2014/foto-tyde.jpg',
+            src: '../fotos/2014/foto-tyde.jpg',
             w: 1000,
             h: 668
         },
         {
-            src: '../Fotos/2014/fotohaven.jpg',
+            src: '../fotos/2014/fotohaven.jpg',
             w: 1000,
             h: 563
         },
         {
-            src: '../Fotos/2014/fotostaal.jpg',
+            src: '../fotos/2014/fotostaal.jpg',
             w: 1000,
             h: 563
         },
         {
-            src: '../Fotos/2014/fotostrand1.jpg',
+            src: '../fotos/2014/fotostrand1.jpg',
             w: 1000,
             h: 563
         },
         {
-            src: '../Fotos/2014/fotostrand2.jpg',
+            src: '../fotos/2014/fotostrand2.jpg',
             w: 1000,
             h: 563
         },
         {
-            src: '../Fotos/2014/fotostrand3.jpg',
+            src: '../fotos/2014/fotostrand3.jpg',
             w: 1000,
             h: 563
         },
         {
-            src: '../Fotos/2014/fotostrand4.jpg',
+            src: '../fotos/2014/fotostrand4.jpg',
             w: 1000,
             h: 541
         },
         {
-            src: '../Fotos/2014/fotostrand5.jpg',
+            src: '../fotos/2014/fotostrand5.jpg',
             w: 1000,
             h: 561
         },
         {
-            src: '../Fotos/2014/fotozbos.jpg',
+            src: '../fotos/2014/fotozbos.jpg',
             w: 1000,
             h: 562
         }
