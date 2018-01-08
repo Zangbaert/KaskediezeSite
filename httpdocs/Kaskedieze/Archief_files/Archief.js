540ae2dc96915a0044f5e8603916fe4ba0cbd573
Timo <tkrijnen@xs4all.nl>
2018-01-08 15:25:49 +0100

    httpdocs opgeruimd; kerstfoto's 2017 toegevoegd; agenda aangepast
    foto’s wormerveer verhuist naar dia-formaat.
    Archief details aangepast.

--- httpdocs/Kaskedieze/Archief_files/Archief.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20170409
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Archief_files/ArchiefMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');detectBrowser();fixAllIEPNGs('Media/transparent.gif');Widget.onload();fixupAllIEPNGBGs();performPostEffectsFixups()}
-function onPageUnload()
-{Widget.onunload();}
